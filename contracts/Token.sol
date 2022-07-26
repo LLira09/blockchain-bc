4c8fff2e1c4f52637ebc6bc33f2f1fd65d53f80b
Jake Lira <jake@Jakes-MBP.attlocal.net>
2022-07-26 15:17:04 -0500

    Token transfers

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -13,6 +13,8 @@ contract Token {
     // Track Balances
     mapping(address => uint256) public balanceOf;
 
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
     constructor(
         string memory _name,
         string memory _symbol,
@@ -22,5 +24,21 @@ contract Token {
         symbol = _symbol;
         totalSupply = _totalSupply * (10**decimals);
         balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        require(balanceOf[msg.sender] >= _value);
+        require(_to != address(0));
+
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+        balanceOf[_to] = balanceOf[_to] + _value;
+
+        // Emit Event
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
